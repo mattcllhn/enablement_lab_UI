3d06bbdf27c59724c2793f4afa3f940175e06b4b
Matt Callahan <mattcllhn@gmail.com>
2017-01-04 11:31:20 -0600

    customer page refactored

--- js/customer.js
+++ js/customer.js
@@ -1,7 +1,7 @@
 /**
- * getUser parses the username from the URL and uses jQuery.getJSON()
- * to get the user data. It will fill in the first name
- */
+* getUser parses the username from the URL and uses jQuery.getJSON()
+* to get the user data. It will fill in the first name
+*/
 //function getUser() {
 //  var username = GetURLParameter('username');
 //  $.getJSON('/RestService/User/username/' + username,
@@ -12,23 +12,23 @@
 //}
 
 /**
- * adds customers to JSON and sends POST to the service
- * @returns
- */
+* adds customers to JSON and sends POST to the service
+* @returns
+*/
 
-function addDependents() {
-var dependentArray = [];
+function addCustomers() {
+	var dependentArray = [];
 
-var count = $('#dependents').val();
-for (var i = 0; i < count; i++) {
-	//grab dependent template
-	var dependent = $('#dependentTemplate').clone();
-	//change id and remove hidden class
-	dependent.attr('id', 'dependent-'+(i+1)).removeClass('hidden');
-	//set html of each legend tag
-	dependent.find('legend').html('Dependent - '+(i+1));
-	var label = dependent.find('label');
-	//loop through each label tag
+	var count = $('#dependents').val();
+	for (var i = 0; i < count; i++) {
+		//grab dependent template
+		var dependent = $('#dependentTemplate').clone();
+		//change id and remove hidden class
+		dependent.attr('id', 'dependent-'+(i+1)).removeClass('hidden');
+		//set html of each legend tag
+		dependent.find('legend').html('Dependent - '+(i+1));
+		var label = dependent.find('label');
+		//loop through each label tag
 		label.each(function(){
 			//grab the "for" attribute string of each label
 			var labelString = $(this).attr('for');
@@ -40,76 +40,79 @@ for (var i = 0; i < count; i++) {
 			$(this).attr('for',labelString);
 			formInput.attr('id', labelString);
 		});//forEach
-	dependentArray.push(dependent);
-	$('#outputDiv').html(dependentArray);
-}//for loop
-
+		dependentArray.push(dependent);
+		$('#outputDiv').html(dependentArray);
+	}//for loop
 }//addCustomers
+
+
+function getCustomers(){
+	var customers = [];
+	var employee = {
+		fname:$('#employeefirstname').val(),
+		lname:$('#employeelastname').val(),
+		bdate:$('#employeebirthdate').val(),
+		gnd:$('#employeegender').val(),
+		sn:$('#employeessn').val(),
+		headOfHousehold:true
+	};//employee
+	customers.push(employee);
+	for (var i = 0; i < $('#dependents').val(); i++) {
+	var dependent = {
+		fname:$('#dependentfirstname'+(i+1)).val(),
+		lname:$('#dependentlastname'+(i+1)).val(),
+		bdate:$('#dependentbirthdate'+(i+1)).val(),
+		gnd:$('#dependentgender'+(i+1)).val(),
+		sn:$('#dependentssn'+(i+1)).val(),
+		headOfHousehold:false
+	};//dependent
+	customers.push(dependent);
+}//for loop
+// console.log('customers',customers);
+return customers;
+
+	// console.log('employee\n\n',employee);
+}//getCustomers
+
+
+
+
 
 $(document).ready(function() {
 
-  $('#dependents').change(function() {
-    addDependents();
-  });//dependents on change
+	$('#dependents').change(function() {
+		addCustomers();
+	});
 
-  $("#saveCustomer").click(
-    function(){
-      var sponsor = GetURLParameter('code');
-      var username = GetURLParameter('username');
-      var form = document.getElementById('customerform');
-      var customers = document.getElementById("customers");
-      var count = form['dependents'].value;
-      var name = form['addrname'].value;
-      var line1 = form['line1'].value;
-      var line2 = form['line2'].value;
-      var city = form['city'].value;
-      var state = form['state'].value;
-      var zip = form['postalCode'].value;
-      var billing = {'name':name, 'line1':line1, 'line2':line2, 'city':city, 'state':state, 'postalCode':zip};
-      var sendInfo = {
-    	      'policyID': null,
-    	      'customers': [],
-    	      'billingAddress': billing,
-    	      'sponsor': sponsor
-    	    };
-      // first we add the employee
-      var fname = form['employeefirstname'].value;
-      var lname = form['employeelastname'].value;
-      var bdate = form['employeebirthdate'].value;
-      var gnd = form['employeegender'].value;
-      var sn = form['employeessn'].value;
-      var employee = {'first':fname,'last':lname,'birthDate':bdate,'headOfHousehold':true,'gender':gnd,'ssn':sn};
-      sendInfo.customers.push(employee);
-      for (i=0; i < count; i++) {
-        var first = form['first' + i].value;
-        var last = form['last' + i].value;
-        var birth = form['birthdate' + i].value;
-        var ssn = form['ssn' + i].value;
-        var gender = form['gender' + i].value;
-        var ct = {'first':first,'last':last,'birthDate':birth,'headOfHousehold':false,'gender':gender,'ssn':ssn};
-        sendInfo.customers.push(ct);
-					console.log(sendInfo);
-      }
-      //alert('save the customer data');
-      $.ajax(
+	$("#saveCustomer").click(
+		function(){
+			var sendInfo = {
+				policyID:null,
+				customers:getCustomers(),
+				sponsor :GetURLParameter('code')
 
-              {
-                url: "/RestService/Policy/username/" + username,
-            	type: "POST",
-            	data: JSON.stringify(sendInfo),
-            	contentType: 'application/json; charset=utf-8',
-            	dataType: 'json',
-            	async: true,
-            	success: function(jsondata) {
-            	  sessionStorage.setItem('policy',JSON.stringify(jsondata));
-            	  // go to the next page to gather the customer data
-            	  window.location = 'questions.html?username=' + username + '&policyid=' + jsondata.policyID;
-            	},
-            	error: function(msg) {
-            	  var rspJson = $.parseJSON(msg.responseText);
-            	  alert(rspJson.status);
-            	}
-              }
-            );//ajax
-    });
-});//docready
+			};//sendinfo
+
+			console.log('sendInfo logged here \n',sendInfo);
+
+			$.ajax(
+			        {
+			          url: "/RestService/Policy/username/" + username,
+			      	type: "POST",
+			      	data: JSON.stringify(sendInfo),
+			      	contentType: 'application/json; charset=utf-8',
+			      	dataType: 'json',
+			      	async: true,
+			      	success: function(jsondata) {
+			      	  sessionStorage.setItem('policy',JSON.stringify(jsondata));
+			      	  // go to the next page to gather the customer data
+			      	  window.location = 'questions.html?username=' + username + '&policyid=' + jsondata.policyID;
+			      	},
+			      	error: function(msg) {
+			      	  var rspJson = $.parseJSON(msg.responseText);
+			      	  alert(rspJson.status);
+			      	}
+			        }
+			      );
+		});//saveCustomer onclick
+	});//docready
